0a62aa3c88e43ad0362f23a01103803608d5c0c6
Jay F Jones <jay.f.jones@gmail.com>
2018-11-24 21:51:36 -0500

    Crappy prototype one.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Rock Paper Scissor Lizard Spock</span>
+        <span class="font-weight-light px-4">THE DRAFT</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/JayFJones/rpsls"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Github</span>
+        <v-icon>cancel</v-icon>
       </v-btn>
     </v-toolbar>
 
